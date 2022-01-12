636726aa7fbae789c118dafa672addc4438cb6cf
Alexcc <gambeta.sw@gmail.com>
2022-01-12 13:30:06 -0400

    Inicio de proyecto kotlin con git

---
+++ app/src/main/java/com/cursokotlin/proyectokotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cursokotlin.proyectokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
